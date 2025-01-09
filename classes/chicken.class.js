fa5cea033cf454cd0db7591b39b7b1d28fda72e8
Stephen Schaz <stephenschaz90@gmail.com>
2025-01-09 23:53:31 +0100

    Working on Classes

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,7 +1,7 @@
 class Chicken extends MovableObject {
 
 
-    constructor(posX = 200 + Math.random() * 500, posY = 405, width = 50, height = 50) {
+    constructor(posX = 200 + Math.random() * 500, posY = 355, width = 100, height = 100) {
         super().loadImage('../assets/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
         this.posX = posX;
         this.posY = posY;
--- classes/movableObject.class.js
+++ classes/movableObject.class.js
@@ -1,9 +1,9 @@
 class MovableObject {
-    posX = 120;
-    posY = 255;
+    posX = 50;
+    posY = 110;
     img;
-    width = 100;
-    height = 200;
+    width = 140;
+    height = 355;
 
     loadImage(path) {
         this.img = new Image();
